85dc4270090dc3ca68e66a9a47c57c16baf8f5b9
wnamen <williamjosephnamen@gmail.com>
2016-09-14 16:32:21 -0700

    completed exercise

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,4 +1,26 @@
 console.log('app.js loaded!');
-
+var word = "hello";
+var words = ['hello','chicken','cow','house',"elephant",'pork','table','vehicle','angular','ruby','wooden'];
+function nextWord(words) {
+  if (words.indexOf(word) === words.length-1) {
+    return word = words[0];
+  }
+  return word = words[words.indexOf(word) + 1];
+}
 // initialize the application
 var app = angular.module("hangmanApp", []);
+app.controller('hangmanCtrl', hangmanCtrl);
+
+function hangmanCtrl(){
+  var vm = this;
+
+  vm.game = new HangmanGame(word);
+  vm.guessLetter = function(){
+    vm.game.guess(vm.input.toLowerCase());
+    vm.input = "";
+  };
+  vm.reset = function(){
+    nextWord(words);
+    vm.game = new HangmanGame(word);
+  }
+}
